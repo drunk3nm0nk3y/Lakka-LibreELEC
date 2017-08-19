746ae37a926c50f441a579fc2685669e29ea11d6
drunk3nm0nk3y <drunk3nm0nk3y>
2017-08-19 19:37:49 +0200

    Fixed RTL8723BU drivers package.mk and added to ODROID-C2 options

--- packages/linux-drivers/rtl8723bu/package.mk
+++ packages/linux-drivers/rtl8723bu/package.mk
@@ -16,19 +16,42 @@
 #  along with OpenELEC.  If not, see <http://www.gnu.org/licenses/>.
 ################################################################################
 
-PKG_NAME="rtl8723bu-master"
+PKG_NAME="rtl8723bu"
 PKG_VERSION="1.0"
 #PKG_ARCH="i386 x86_64"
 PKG_LICENSE="GPL"
 PKG_SITE="https://github.com/lwfinger/rtl8723bu"
 PKG_URL="https://github.com/lwfinger/rtl8723bu/archive/master.zip"
-PKG_SOURCE_DIR="$PKG_NAME"
+PKG_SOURCE_DIR="rtl8723bu-master"
 PKG_DEPENDS_TARGET="toolchain linux"
 PKG_NEED_UNPACK="$LINUX_DEPENDS"
 PKG_SECTION="driver"
 PKG_SHORTDESC="Linux RTL8723BU drivers"
 PKG_LONGDESC="Linux RTL8723BU drivers"
+
 PKG_IS_ADDON="no"
 PKG_AUTORECONF="no"
 
+if [ "$TARGET_KERNEL_ARCH" = "arm64" -a "$TARGET_ARCH" = "arm" ]; then
+  PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET gcc-linaro-aarch64-elf:host"
+  export PATH=$ROOT/$TOOLCHAIN/lib/gcc-linaro-aarch64-elf/bin/:$PATH
+  TARGET_PREFIX=aarch64-elf-
+fi
+
+pre_make_target() {
+  unset LDFLAGS
+}
+
+make_target() {
+  make V=1 \
+       ARCH=$TARGET_KERNEL_ARCH \
+       KSRC=$(kernel_path) \
+       CROSS_COMPILE=$TARGET_PREFIX \
+       CONFIG_POWER_SAVING=n
+}
+
+makeinstall_target() {
+  mkdir -p $INSTALL/usr/lib/modules/$(get_module_dir)/$PKG_NAME
+    cp *.ko $INSTALL/usr/lib/modules/$(get_module_dir)/$PKG_NAME
+}
 
